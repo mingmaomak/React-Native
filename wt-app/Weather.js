4fa46937e01642a9023cb5b3b8551501e008c81b
Jo <mingmaomak@gmail.com>
2022-08-26 08:57:45 +0700

    xd

--- wt-app/Weather.js
+++ wt-app/Weather.js
@@ -1,5 +1,24 @@
+
 export default function Weather(props) {
- return (
- <Text>{props.zipCode}</Text>
- );
+const [forecastInfo, setForecastInfo] = useState({
+main: '-',
+description: '-',
+temp: 0
+})
+return (
+<View>
+<ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
+<Text>Zip Code</Text>
+<Text>{props.zipCode}</Text>
+<Forecast {...forecastInfo} />
+</ImageBackground>
+</View>
+);
 }
+const styles = StyleSheet.create({
+backdrop: {
+alignItems: 'center',
+width: '100%',
+height: '100%'
+},
+});
