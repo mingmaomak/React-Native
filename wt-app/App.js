8c5fdffaea91286d4cac06c7a589ea39cc2ba078
Jo <mingmaomak@gmail.com>
2022-08-26 08:44:30 +0700

    ayo

--- wt-app/App.js
+++ wt-app/App.js
@@ -2,11 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const doIt = () => {
+    console.log("Hello from console")
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+<View style={styles.container}>
+ <Weather zipCode="90110"/>
+ <StatusBar style="auto" />
+ </View>
   );
 }
 
---
+++ wt-app/Forcast.js
@@ -0,0 +1,12 @@
+export default function Forecast(props) {
+ return (
+ <View >
+ <Text>{props.main}</Text>
+ <Text>{props.description}</Text>
+ <View>
+ <Text>{props.temp}</Text>
+ <Text>°C</Text>
+ </View>
+ </View>
+ );
+}
